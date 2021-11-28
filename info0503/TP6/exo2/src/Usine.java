a395ef540740a39c9ab21e49715d68881601d84a
NicolasMACZYTA <45238131+NicolasMACZYTA@users.noreply.github.com>
2021-11-28 18:19:05 +0100

    switcheroo

--- info0503/TP6/exo2/src/Usine.java
+++ info0503/TP6/exo2/src/Usine.java
@@ -1,47 +1,83 @@
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
 
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
- * Classe correspondant à un serveur TCP multithreadé.
+ * Classe correspondant à un client TCP.
  * Le client envoie la chaine 'Bonjour' et lit une réponse de la part du serveur.
  * Le client envoie ensuite la chaine 'Au revoir' et lit une réponse.
- * Le numéro de port du serveur est spécifié dans la classe ServeurTCP.
+ * Le numéro de port du serveur est spécifié dans la classe ServeurTCPMulti.
  * @author Cyril Rabat
  */
 public class Usine {
-
+    
     public static final int portEcoute = 5101;
 
     public static void main(String[] args) {
-        // Création de la socket serveur
-        ServerSocket socketServeur = null;
-        try {    
-            socketServeur = new ServerSocket(portEcoute);
+        // Création de la socket
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", portEcoute);
+        } catch(UnknownHostException e) {
+            System.err.println("Erreur sur l'hôte : " + e);
+            System.exit(0);
         } catch(IOException e) {
             System.err.println("Création de la socket impossible : " + e);
             System.exit(0);
         }
 
-        // Attente des connexions des clients
+        // Association d'un flux d'entrée et de sortie
+        BufferedReader input = null;
+        PrintWriter output = null;
         try {
-            Socket socketClient;
-            while(true) {
-            socketClient = socketServeur.accept();
-            ThreadConnexion t = new ThreadConnexion(socketClient);
-            t.start();
-            }
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
         } catch(IOException e) {
-            System.err.println("Erreur lors de l'attente d'une connexion : " + e);
+            System.err.println("Association des flux impossible : " + e);
             System.exit(0);
         }
-    
-        // Fermeture de la socket
+
+        // Envoi de 'Bonjour'
+        String message = "Bonjour";
+        System.out.println("Envoi: " + message);
+        output.println(message);
+
+        // Lecture de 'Bonjour'
         try {
-            socketServeur.close();
+            message = input.readLine();
         } catch(IOException e) {
-            System.err.println("Erreur lors de la fermeture de la socket : " + e);
+            System.err.println("Erreur lors de la lecture : " + e);
+            System.exit(0);
+        }
+        System.out.println("Lu: " + message);
+
+        // Envoi de 'Au revoir'
+        message = "Au revoir";
+        System.out.println("Envoi: " + message);
+        output.println(message);
+
+        // Lecture de 'Au revoir'
+        try {
+            message = input.readLine();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la lecture : " + e);
+            System.exit(0);
+        }
+        System.out.println("Lu: " + message);
+
+        // Fermeture des flux et de la socket
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la fermeture des flux et de la socket : " + e);
             System.exit(0);
         }
     }

e6da04f7c70b19b522e0d809479e325a67500750
NicolasMACZYTA <45238131+NicolasMACZYTA@users.noreply.github.com>
2021-11-28 19:32:09 +0100

    *grognements sourds*

--- info0503/TP6/exo2/src/Usine.java
+++ info0503/TP6/exo2/src/Usine.java
@@ -8,6 +8,8 @@ import Voitures.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import org.json.JSONObject;
+
 
 /**
  * @author Cyril Rabat
@@ -18,6 +20,16 @@ public class Usine {
     public static final int portEcoute = 5101;
 
     public static void main(String[] args) {
+        Moteur[] tMoteur = {new Moteur("XUD","diesel",90), new Moteur("XU","essence",92), new Moteur("PRV_REV2","essence",205)};
+        String[] couleurs = {"vert anglais","gris métallisé", "noir laqué"};
+        Option[] options = {new Option("climatisation", "climatisation"), new Option("pack confort", "sieges velours baguettes en ronce de noyer"), new Option("autoradio CD", "autoradio CD") };
+        Modele modele = new Modele("406 berline");
+        modele.setCouleurs(couleurs);
+        modele.setMoteurs(tMoteur);
+        modele.setOptions(options);
+
+
+
         // Création de la socket
         Socket socket = null;
         try {
@@ -58,7 +70,10 @@ public class Usine {
         // Envoi des voitures construites
 
         //TODO : construction de plusieurs objets voiture
-        message = "voiture a (rajouter plus tard)";
+
+        Voiture v1 = new Voiture("AHJKDY05462JHSDF", modele, modele.getMoteurs()[0], modele.getCouleurs()[1], modele.getOptions(),"04/01/1995");
+        
+        message = ""+new JSONObject(v1);
         System.out.println("Envoi: " + message);
         output.println(message);
 

ca6e07efe43170a1076289b4fe79680ef870f615
NicolasMACZYTA <45238131+NicolasMACZYTA@users.noreply.github.com>
2021-11-28 18:10:57 +0100

    Summary

--- info0503/TP6/exo2/src/testVoiture.java
+++ info0503/TP6/exo2/src/testVoiture.java
@@ -12,7 +12,7 @@ public class testVoiture {
         Moteur[] tMoteur = {new Moteur("XUD","diesel",90), new Moteur("XU","essence",92), new Moteur("PRV_REV2","essence",205)};
         String[] couleurs = {"vert anglais","gris métallisé", "noir laqué"};
         Option[] options = {new Option("climatisation", "climatisation"), new Option("pack confort", "sieges velours baguettes en ronce de noyer"), new Option("autoradio CD", "autoradio CD") };
-        Usine u = new Usine("JJLand", 1, 250, 250);
+        //Usine u = new Usine("JJLand", 1, 250, 250);
         Modele modele = new Modele("406 berline");
         modele.setCouleurs(couleurs);
         modele.setMoteurs(tMoteur);
@@ -22,9 +22,9 @@ public class testVoiture {
 
         System.out.println(v1.toString());
         System.out.println("\n stockage : ");
-        u.stockerVoiture(v1, 'A', 1);
-        v2 = u.parking.getVoiture('A', 1);
-        System.out.println(v2.toString());
+        //u.stockerVoiture(v1, 'A', 1);
+        //v2 = u.parking.getVoiture('A', 1);
+        //System.out.println(v2.toString());
 
         //TEST JSON
         System.out.println("Usine\n");

3453384b213d2f5f6892e768503dadbd0b484e40
FluffiestFloof <thebluewulf@gmail.com>
2022-01-04 19:16:30 +0100

    agrougrou

--- info0603/TP1/td0ex6.py
+++ info0603/TP1/td0ex6.py
@@ -7,6 +7,16 @@ import doctest as dt
 
 def gne():
     """jesaispas"""
+    
+#Sans aucune justification, proposer un ordre de grandeur de la probabilité que deux personnes de cette
+#salle aient la même date d’anniversaire.
+
+#Donner une représentation et/ou un code informatique calculant/estimant cette probabilité.
+
+#A partir de combien de personnes présentent cette probabilité dépasse 0,5 ? 0,9 ?
+
+
+#Wesh c'est quoi ça moi je suis pas magicien
 
     
 if __name__ == "__main__":

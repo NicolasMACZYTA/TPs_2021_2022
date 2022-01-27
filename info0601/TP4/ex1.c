5eb033e2c5a605c7f26afb63441b9aed4a265b2f
Fluffiest Floof <lug.brunet@etudiant.univ-reims.fr>
2022-01-27 10:29:09 +0100

    mdr

--- info0601/TP4/ex1.c
+++ info0601/TP4/ex1.c
@@ -9,13 +9,28 @@
 
 int main(int argc, char *argv[]){
     int nbr = atoi(argv[1]);
-    pid_t pid[nbr];
-    int i = 0;
+    pid_t *pid = malloc(sizeof(pid)*nbr);
+    int i = 0, statut = 0;
+    
     printf("Il y a %d fils.", nbr);
 
     for(i = 0; i<nbr; i++){
-        if(fork()==0){
-
-        }else
+        if((pid[i]=fork())==0){
+            if(getpid()!=getppid()){
+                srand(time(NULL));
+                printf("Fils%d : PID=%d, rand=%d\n", i+1, getpid(), rand());
+                exit(EXIT_SUCCESS);
+            }
+        }else if(pid[i]<0){
+            printf("Erreur creation processus %d", i);
+            exit(EXIT_FAILURE);
+        }else{
+            if(wait(&statut)==-1){
+                perror("Erreur attente fils");
+                exit(EXIT_FAILURE);
+            }
+        }
     }
+
+    return EXIT_SUCCESS;
 }

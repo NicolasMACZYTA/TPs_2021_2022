a074bf346efaaaca25cc5ffd74296e5b534cb6a6
nithrilh <nithrilh@debian-BULLSEYE-live-builder-AMD64>
2022-02-08 08:51:53 +0100

    cestmabite

--- info0603/TP4-5-6/ChiffreurFeistel.py
+++ info0603/TP4-5-6/ChiffreurFeistel.py
@@ -1,60 +1,56 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  3 18:10:52 2022
-
-@author: nithrilh
-"""
-
-from CodeurCA import *
+from random import *
 from Binaire603 import *
+from CodeurCA import *
 from Texte603 import *
-from ElmtZnZ import *
-class ChiffrementAffine(CodeurCA):# Ne pas toucher
+from FBijFeistel import *
+
+
+class ChiffreurFeistel(CodeurCA):# Ne pas toucher
     """Un codeur doit surcharger les méthodes __init__ __repr__ __str__
     binCode, binDecode et codeurTest
     renvoyant et recevant un Binaire603
-    C'est une forme de classe abstraites"""
-    def __init__(self,a=3,b=5):
-        """initialisation du chiffrement affine"""
-        self.a = ElmtZnZ(a,256)
-        assert self.a.estInversible()
-        self.b = b
-        
+    C'est une forme de classe abstraite"""
+    def __init__(self, f, cle, nb_tours):
+        self.fbij=FBijFeistel(f, cle, nb_tours)
+
     def __str__(self):
-        return "Chiffreur Affine"
+        return (f"Chiffreur de Feistel avec f={self.fbij}")
     
     def __repr__(self):
-        return f'ChiffrementAffine()'
-
+        return(f"ChiffreurFeistel({self.f}, {self.cle}, {self.nb_tours})")
+    
     def binCode(self,monBinD:Binaire603)->Binaire603:
         """
-        >>> ChiffrementAffine(3,1).binCode(Binaire603([ 1,10,150]))
-        Binaire603([ 0x04, 0x1f, 0xc3])
         """
-        bc=FBijFeistel(f4b1,5,16)(monBinD)
-            
-        return Binaire603(bc)
+        assert isinstance(monBinD, Binaire603)
         
+        retour=[]
+        for elem in monBinD:
+                retour.append(self.fbij(elem))
+
+        return Binaire603(retour)
+    
     def binDecode(self,monBinC:Binaire603)->Binaire603:
         """
-        >>> ChiffrementAffine(3,1).binDecode(Binaire603([ 0x04, 0x1f, 0xc3]))
-        Binaire603([1,10,150])
         """
-        ld=[((bd-self.b)*self.a.inverse().a)%256 for bd in monBinC]
-        
-        return ld
+        assert isinstance(monBinC, Binaire603)
+        retour=[]
+        for elem in monBinC:
+                retour.append(self.fbij.valInv(elem))
 
-
+        return Binaire603(retour)
+    
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-    monCodeur=ChiffrementAffine(3,5) #A modifier si repris dans une classe en héritant
-    for k in range(5):
-        monBin=Binaire603.exBin603(num=k,taille=25)
-        print("Bin:",monBin)
-        monBinCr=monCodeur.binCode(monBin)
-        print("Bin Codée:",monBinCr)
-        print("monBinCr décodé est égal à Monbin ?",monCodeur.binDecode(monBinCr)==monBin)
-
-
+    #for k in range(1,17):
+     #   FBijFeistel(f4b1, 113, k).afficheGraphique()
+    bin=Texte603("dinosaure").toBinaire603()
+    c=ChiffreurFeistel(f4b1, 0x0101, 2)
+    binC=c.binCode(bin)
+    print(Texte603(binC))
+    binD=c.binDecode(binC)
+    texte=Texte603("Ï7ƗƔċã#´Ɣ£").toBinaire603()
+    print(Texte603(c.binDecode(texte)))
+    print(Texte603(binD))
+    
